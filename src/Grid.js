30c9324058546ef3d294e7fb78017a89a3bdb5fd
nikola-wd <nikola_wd@hotmail.com>
2022-04-18 17:47:03 +0200

    Initials state, reducer and UI finished

--- src/Grid.js
+++ src/Grid.js
@@ -1,20 +1,87 @@
-import React, { Fragment } from 'react';
+import React, { useState, useReducer } from 'react';
 import Cell from './Cell';
+
+const CASES = {
+  UP: 'UP',
+  DOWN: 'DOWN',
+  LEFT: 'LEFT',
+  RIGHT: 'RIGHT',
+};
 
 const cell_size = 80;
 const grid_size = 3;
 
-const Grid = () => {
-  return (
-    <div
-      className="grid"
-      style={{ '--grid-size': grid_size, '--cell-size': cell_size }}
-    >
-      {[...Array(grid_size * grid_size).keys()].map((_, index) => {
-        const number = Math.random() <= 0.5 ? 0 : Math.random() <= 0.5 ? 2 : 4;
+const initialReducerState = [
+  [0, 2, 2],
+  [0, 0, 0],
+  [0, 0, 0],
+];
 
-        return <Cell key={index} number={number} />;
-      })}
+const logicReducer = (state, action) => {
+  const updatedState = [...state];
+
+  switch (action.type) {
+    case CASES.UP:
+      console.log('swiped up');
+      return [...updatedState];
+    case CASES.DOWN:
+      console.log('swiped DOWN');
+      return [...updatedState];
+    case CASES.LEFT:
+      console.log('swiped LEFT');
+      return [...updatedState];
+    case CASES.RIGHT:
+      console.log('swiped RIGHT');
+      return [...updatedState];
+    default:
+      return updatedState;
+  }
+};
+
+const Grid = () => {
+  const [clicked, setClicked] = useState(false);
+  const [state, dispatch] = useReducer(logicReducer, initialReducerState);
+
+  const [isSwipable, setIsSwipable] = useState(true);
+  // const [offsetCounter, setOffsetCounter] = useState(0);
+  const [initialXY, setInitialXY] = useState([null, null]);
+  const [lastXY, setLastXY] = useState([null, null]);
+
+  const handleMouseMove = (e) => {
+    if (!clicked || !isSwipable) return;
+    if (lastXY[0] !== null) return;
+
+    const { clientX, clientY } = e;
+
+    setLastXY([clientX, clientY]);
+
+    console.log(clientX, clientY);
+    return;
+  };
+
+  return (
+    <div>
+      <h2> {clicked ? 'CLICKED' : 'NOT CLICKED'}</h2>
+      <h3>InitialXY: {JSON.stringify(initialXY)}</h3>
+      <h3>LastXY: {JSON.stringify(lastXY)}</h3>
+
+      <div
+        className="grid"
+        style={{ '--grid-size': grid_size, '--cell-size': cell_size }}
+        onMouseDown={(e) => {
+          setClicked(true);
+          setInitialXY([e.clientX, e.clientY]);
+        }}
+        onMouseUp={(e) => {
+          setClicked(false);
+        }}
+        onMouseMove={handleMouseMove}
+        onMouseLeave={() => setClicked(false)}
+      >
+        {[...state.flat()].map((arrNumber, index) => {
+          return <Cell key={index} number={arrNumber} />;
+        })}
+      </div>
     </div>
   );
 };
